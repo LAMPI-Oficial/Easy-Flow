425864a21d19c5fb486a7fff662bbba87432bef0
Andeson Silva <andsilva607@gmail.com>
2023-03-25 14:15:11 -0300

    FE.1.5 Trabalhando com responsividade nos comunicados

--- lib/layers/modules/home/pages/announcements.dart
+++ lib/layers/modules/home/pages/announcements.dart
@@ -6,18 +6,45 @@ class PageAnnouncements extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-      child: ListView.separated(
-        itemBuilder: (context, index) {
-          return const CardAnnoumencementsWidget(
-              text:
-                  "Aquisição de macbooks para o laboratório gera entusiasmo para os desenvolvedores.");
-        },
-        separatorBuilder: (context, index) =>
-            const Padding(padding: EdgeInsets.all(8)),
-        itemCount: 5,
-      ),
+    return LayoutBuilder(
+      builder: (p0, p1) {
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          child: GridView.builder(
+            itemCount: 10,
+            physics: const BouncingScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisExtent: 240,
+              crossAxisCount: qtdeColumn(p1.maxWidth),
+              mainAxisSpacing: 12,
+              crossAxisSpacing: 12,
+            ),
+            itemBuilder: (context, index) {
+              return CardAnnoumencementsWidget(
+                text:
+                    "Aquisição de macbooks para o laboratório gera entusiasmo para os desenvolvedores." *
+                        10,
+              );
+            },
+          ),
+        );
+      },
     );
+  }
+
+  int qtdeColumn(double largura) {
+    int column = 1;
+    if (largura < 500) {
+      column = 1;
+    } else if (largura < 800) {
+      column = 2;
+    } else if (largura < 1000) {
+      column = 3;
+    } else if (largura < 1200) {
+      column = 4;
+    } else if (largura >= 1200) {
+      column = 5;
+    }
+    return column;
   }
 }
--- lib/layers/modules/home/widgets/card_announcements_widget.dart
+++ lib/layers/modules/home/widgets/card_announcements_widget.dart
@@ -7,81 +7,92 @@ class CardAnnoumencementsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                      "assets/images/noticias.png",
-                    ),
-                  ),
-                ),
-                height: 150,
-                width: 280,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: ListTile(
-                contentPadding: EdgeInsets.zero,
-                leading: CircleAvatar(
-                  maxRadius: 25,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        "GDPPI",
-                        style: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
+    return LayoutBuilder(
+      builder: (p0, p1) {
+        return Card(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(
+                          "assets/images/noticias.png",
                         ),
                       ),
-                      Text(
-                        "Grupo discente de programação\n e projetos inovadores ",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 3,
+                    ),
+                    height: 150,
+                    width: 280,
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: CircleAvatar(
+                        maxRadius: 25,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              "GDPPI",
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Grupo discente de programação\n e projetos inovadores ",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 3,
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
+                      ),
+                      title: SingleChildScrollView(
+                        child: ExpandableText(
+                          text,
+                          maxLines: 3,
+                          animation: true,
+                          textAlign: TextAlign.justify,
+                          expandText: "Ver mais...",
+                          collapseText: "\nVer menos",
+                          linkStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                            color: Color(0xFF000000),
+                          ),
+                          style: const TextStyle(
+                            fontSize: 13,
+                            color: Color(0xFF000000),
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Segoe UI',
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                title: ExpandableText(
-                  text,
-                  maxLines: 3,
-                  textAlign: TextAlign.justify,
-                  expandText: "\nVer mais...",
-                  collapseText: "\nVer menos",
-                  linkStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 13,
-                    color: Color(0xFF000000),
-                  ),
-                  style: const TextStyle(
-                    fontSize: 13,
-                    color: Color(0xFF000000),
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'Segoe UI',
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
