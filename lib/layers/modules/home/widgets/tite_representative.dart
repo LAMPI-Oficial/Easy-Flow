c06043bd65a1b0ac5111cc659c3de04ffcceaef4
Andeson Silva <andsilva607@gmail.com>
2023-03-25 23:27:23 -0300

    FE.1.5 Alterações finais

--- lib/layers/modules/home/widgets/tite_representative.dart
+++ lib/layers/modules/home/widgets/tite_representative.dart
@@ -29,62 +29,82 @@ class TileRepresentativeWidget extends StatelessWidget {
           ),
         ),
       ),
-      title: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-        margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                AutoSizeText(
-                  representative.name,
-                  maxLines: 1,
-                  maxFontSize: 14,
-                  style: const TextStyle(
-                      fontSize: 14,
-                      // overflow: TextOverflow.ellipsis,
-                      color: Color(
-                        0xFF4A6F91,
-                      ),
-                      fontFamily: 'Segoe UI'),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 8.0, right: 2),
-                  child: Icon(
-                    Icons.verified_outlined,
-                    size: 10,
-                    color: Color(0xFF0085FF),
+      title: Card(
+        elevation: 0,
+        margin: const EdgeInsets.symmetric(vertical: 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            8,
+          ),
+        ),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: AutoSizeText(
+                      representative.name,
+                      maxLines: 1,
+                      maxFontSize: 14,
+                      style: const TextStyle(
+                          fontSize: 14,
+                          overflow: TextOverflow.ellipsis,
+                          color: Color(
+                            0xFF4A6F91,
+                          ),
+                          fontFamily: 'Segoe UI'),
+                    ),
                   ),
-                ),
-                AutoSizeText(
-                  representative.area,
-                  style: const TextStyle(
-                    fontSize: 10,
-                    color: Color(0xFF0085FF),
-                    fontFamily: 'Segoe UI',
-                    overflow: TextOverflow.ellipsis,
+                  Expanded(
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 2, right: 2),
+                          child: Icon(
+                            Icons.verified_outlined,
+                            size: 10,
+                            color: Color(0xFF0085FF),
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            child: Text(
+                              representative.area,
+                              style: const TextStyle(
+                                fontSize: 10,
+                                color: Color(0xFF0085FF),
+                                fontFamily: 'Segoe UI',
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                // const Spacer()
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 0.0, bottom: 1),
-              child: SubtitleWidget(
-                text: representative.email,
-                icon: Icons.email_outlined,
+                ],
               ),
-            ),
-            SubtitleWidget(
-              text: representative.turn,
-              icon: Icons.alarm,
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 0.0, bottom: 1),
+                child: SubtitleWidget(
+                  text: representative.email,
+                  icon: Icons.email_outlined,
+                ),
+              ),
+              SubtitleWidget(
+                text: representative.turn,
+                icon: Icons.alarm,
+              )
+            ],
+          ),
         ),
       ),
     );
