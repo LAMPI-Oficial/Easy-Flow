300d8df7e11c273ab83f0e126e111baa43da1a6f
Andeson Silva <andsilva607@gmail.com>
2023-03-26 01:20:37 -0300

    FE.1.5 Aplicando scroll na página de resultado não encontrado

--- lib/layers/modules/home/pages/comp_no_found.dart
+++ lib/layers/modules/home/pages/comp_no_found.dart
@@ -6,51 +6,54 @@ class CompNoFound extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsetsDirectional.symmetric(horizontal: 50),
+      padding:
+          const EdgeInsetsDirectional.symmetric(horizontal: 50, vertical: 10),
+      alignment: Alignment.center,
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10.0),
-            child: Image.asset(
-              "assets/images/scan_delete.png",
-              color: const Color(0xFF4A6F91),
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Image.asset(
+                "assets/images/scan_delete.png",
+                color: const Color(0xFF4A6F91),
+              ),
             ),
-          ),
-          // const
-          const Text.rich(
-            style: TextStyle(
-              fontSize: 20,
-            ),
-            textAlign: TextAlign.center,
-            TextSpan(
-              children: [
-                TextSpan(
-                  text: "Poxa, ainda não ",
-                  style: TextStyle(
-                    color: Color(0xFF4A6F90),
-                    fontWeight: FontWeight.bold,
+            const Text.rich(
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Poxa, ainda não ",
+                    style: TextStyle(
+                      color: Color(0xFF4A6F90),
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                TextSpan(
-                  text: "temos isso que você pesquisou ",
-                  style: TextStyle(
-                    color: Color(0xFF4A6F91),
-                    // fontWeight: FontWeight.bold,
+                  TextSpan(
+                    text: "temos isso que você pesquisou ",
+                    style: TextStyle(
+                      color: Color(0xFF4A6F91),
+                      // fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                TextSpan(
-                  text: "tente outro",
-                  style: TextStyle(
-                    color: Color(0xFF4A6F90),
-                    fontWeight: FontWeight.bold,
+                  TextSpan(
+                    text: "tente outro",
+                    style: TextStyle(
+                      color: Color(0xFF4A6F90),
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
