28b736ddf2bf60b46b6fffde6737bf0b13285d9b
Michael Alves <110749585+Michallves@users.noreply.github.com>
2023-03-27 15:13:39 -0300

    bug fix mesclagem

--- lib/layers/modules/complaint/pages/complaint_page.dart
+++ lib/layers/modules/complaint/pages/complaint_page.dart
@@ -2,7 +2,6 @@ import 'package:easyflow/core/utils/validators_util.dart';
 import 'package:easyflow/layers/modules/complaint/complaint_controller.dart';
 import 'package:easyflow/layers/widgets/text_field_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_file_picker/form_builder_file_picker.dart';
 import 'package:get/get.dart';
 
@@ -22,73 +21,112 @@ class ComplaintPage extends GetView<ComplaintController> {
             padding: const EdgeInsets.all(12.0),
             child: Container(
               alignment: AlignmentDirectional.center,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: TextFieldWidget(
-                                label: "Digite seu nome completo",
-                                hintText: "nome completo",
-                                controller: controller.nameTextController,
-                                keyboardType: TextInputType.name,
-                                prefixIcon: const Icon(Icons.badge_outlined),
-                                textInputAction: TextInputAction.next,
-                                autovalidateMode: AutovalidateMode.onUserInteraction,
-                                validator: (value) => Validators.combine(
-                                  [
-                                    () => Validators.isNotEmpty(value),
-                                    () => Validators.IsName(value)
-                                  ],
-                                ),
-                              ),
-                            ),
-                            TextFieldWidget(
-                              label: "Digite seu e-mail",
-                              hintText: "e-mail",
-                              controller: controller.emailTextController,
-                              prefixIcon: const Icon(Icons.email_outlined),
-                              keyboardType: TextInputType.emailAddress,
-                              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: ListView(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: TextFieldWidget(
+                              label: "Digite seu nome completo",
+                              hintText: "nome completo",
+                              controller: controller.nameTextController,
+                              keyboardType: TextInputType.name,
+                              prefixIcon: const Icon(Icons.badge_outlined),
                               textInputAction: TextInputAction.next,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
                               validator: (value) => Validators.combine(
                                 [
                                   () => Validators.isNotEmpty(value),
-                                  () => Validators.isEmail(value)
+                                  () => Validators.IsName(value)
                                 ],
                               ),
                             ),
-                            const Text("Descreva sua reclamação",
-                                style: TextStyle(
-                                  fontFamily: 'Segoe_UI',
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 8),
-                              child: Text(
-                                "Escreva a sua reclamação com detalhes precisos",
-                                style: TextStyle(
-                                  fontFamily: 'Segoe_UI',
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                ),
+                          ),
+                          TextFieldWidget(
+                            label: "Digite seu e-mail",
+                            hintText: "e-mail",
+                            controller: controller.emailTextController,
+                            prefixIcon: const Icon(Icons.email_outlined),
+                            keyboardType: TextInputType.emailAddress,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            textInputAction: TextInputAction.next,
+                            validator: (value) => Validators.combine(
+                              [
+                                () => Validators.isNotEmpty(value),
+                                () => Validators.isEmail(value)
+                              ],
+                            ),
+                          ),
+                          const Text("Descreva sua reclamação",
+                              style: TextStyle(
+                                fontFamily: 'Segoe_UI',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              )),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8),
+                            child: Text(
+                              "Escreva a sua reclamação com detalhes precisos",
+                              style: TextStyle(
+                                fontFamily: 'Segoe_UI',
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
-                            TextFormField(
-                              controller: controller.descriptionTextController,
-                              keyboardType: TextInputType.multiline,
-                              autovalidateMode: AutovalidateMode.onUserInteraction,
-                              textInputAction: TextInputAction.next,
+                          ),
+                          TextFormField(
+                            controller: controller.descriptionTextController,
+                            keyboardType: TextInputType.multiline,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            textInputAction: TextInputAction.next,
+                            decoration: InputDecoration(
+                              alignLabelWithHint: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: const BorderSide(
+                                  color: Color(0xFFD4D4D4),
+                                ),
+                              ),
+                              labelText: '',
+                            ),
+                            minLines: 6,
+                            maxLines: 6,
+                            validator: (value) => Validators.combine(
+                              [
+                                () => Validators.isNotEmpty(value),
+                                () => Validators.isDescription(value)
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            child: FormBuilderFilePicker(
+                              name: "Arquivo",
+                              previewImages: true,
+                              allowMultiple: true,
+                              maxFiles: 1,
+                              withData: true,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (value) => Validators.combine(
+                                [
+                                  () => Validators.isArchive(value),
+                                ],
+                              ),
+                              onChanged: (val) {},
                               decoration: InputDecoration(
-                                alignLabelWithHint: true,
                                 border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(8),
                                 ),
@@ -98,95 +136,59 @@ class ComplaintPage extends GetView<ComplaintController> {
                                     color: Color(0xFFD4D4D4),
                                   ),
                                 ),
-                                labelText: '',
                               ),
-                              minLines: 6,
-                              maxLines: 6,
-                              validator: (value) => Validators.combine(
-                                [
-                                  () => Validators.isNotEmpty(value),
-                                  () => Validators.isDescription(value)
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 12),
-                              child: FormBuilderFilePicker(
-                                name: "Arquivo",
-                                previewImages: true,
-                                allowMultiple: true,
-                                maxFiles: 1,
-                                withData: true,
-                                autovalidateMode: AutovalidateMode.onUserInteraction,
-                                validator: (value) => Validators.combine(
-                                  [
-                                        () => Validators.isArchive(value),
-                                  ],
-                                ),
-                                onChanged: (val){},
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                    borderSide: const BorderSide(
-                                      color: Color(0xFFD4D4D4),
-                                    ),
-                                  ),
-                                ),
-                                typeSelectors: [
-                                  TypeSelector(
-                                    type: FileType.any,
-                                    selector: Container(
-                                      alignment: AlignmentDirectional.center,
-                                      width: MediaQuery.of(context).size.width * 0.74,
-                                      child: const Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Icon(
-                                            Icons.add_link_outlined,
-                                            color: Colors.blue,
-                                          ),
-                                          Padding(
-                                            padding: EdgeInsets.only(left: 8.0),
-                                            child: Text(
-                                              "Anexar arquivo",
-                                              style: TextStyle(
+                              typeSelectors: [
+                                TypeSelector(
+                                  type: FileType.any,
+                                  selector: Container(
+                                    alignment: AlignmentDirectional.center,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.74,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: const [
+                                        Icon(
+                                          Icons.add_link_outlined,
+                                          color: Colors.blue,
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(left: 8.0),
+                                          child: Text(
+                                            "Anexar arquivo",
+                                            style: TextStyle(
                                                 color: Colors.blue,
-                                                decoration: TextDecoration.underline
-                                              ),
-                                            ),
+                                                decoration:
+                                                    TextDecoration.underline),
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          controller.complaint();
-                        },
-                        style: const ButtonStyle(
-                          backgroundColor:
-                          MaterialStatePropertyAll(Colors.blue),
-                        ),
-                        child: const Text(
-                          "Enviar",
-                          style: TextStyle(color: Colors.white),
-                        ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        controller.complaint();
+                      },
+                      style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                      ),
+                      child: const Text(
+                        "Enviar",
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
