80546635a899b00885db6e987f0c0bc786beddd7
Michael Alves <110749585+Michallves@users.noreply.github.com>
2023-03-31 19:42:43 -0300

    [DEV] - pages horary

--- lib/layers/modules/horary/horary_page.dart
+++ lib/layers/modules/horary/horary_page.dart
@@ -19,125 +19,116 @@ class HoraryPage extends GetView<HoraryController> {
             return Scaffold(
               appBar: AppBar(
                 title: const Text('Horário'),
-                actions: [
-                  GetPlatform.isIOS
-                      ? FloatingActionButton(
-                          elevation: 0,
-                          onPressed: () {},
-                          child: const Icon(Icons.add),
-                        )
-                      : Container(),
-                ],
               ),
-              floatingActionButton: !GetPlatform.isIOS
-                  ? FloatingActionButton(
-                      elevation: 0,
-                      onPressed: () {},
-                      child: const Icon(Icons.add),
-                    )
-                  : Container(),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
               body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: SizedBox(
-                          height: 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              const Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "Mesa",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontFamily: "Segoe UI",
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 13,
-                                    color: Color(0xff4A6F90),
+                child: Center(
+                  child: SizedBox(
+                    width: constraints.maxWidth * .90,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: SizedBox(
+                            height: constraints.maxHeight * .05,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                const Expanded(
+                                  flex: 1,
+                                  child: Text(
+                                    "Mesa",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontFamily: "Segoe UI",
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 13,
+                                      color: Color(0xff4A6F90),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              const Expanded(
-                                flex: 3,
-                                child: Text(
-                                  "Nome",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontFamily: "Segoe UI",
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 13,
-                                    color: Color(0xff4A6F90),
+                                const Expanded(
+                                  flex: 3,
+                                  child: Text(
+                                    "Nome",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontFamily: "Segoe UI",
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 13,
+                                      color: Color(0xff4A6F90),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Center(
-                                  child: DropButtonWidget(
-                                    hint: "Dia",
-                                    value: _.valueDay,
-                                    items: _.valuesDays.map((value) {
-                                      return DropdownMenuItem(
-                                        value: value,
-                                        child: Text(value),
-                                      );
-                                    }).toList(),
-                                    onChanged: (value) =>
-                                        _.changeValueDay(value),
+                                Expanded(
+                                  flex: 2,
+                                  child: Center(
+                                    child: DropButtonWidget(
+                                      hint: "Dia",
+                                      value: _.valueDay,
+                                      items: _.valuesDays.map((value) {
+                                        return DropdownMenuItem(
+                                          value: value,
+                                          child: Text(value),
+                                        );
+                                      }).toList(),
+                                      onChanged: (value) =>
+                                          _.changeValueDay(value),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Center(
-                                  child: DropButtonWidget(
-                                    hint: "Turno",
-                                    value: _.valueTurn,
-                                    items: _.valuesTurns.map((value) {
-                                      return DropdownMenuItem(
-                                        value: value,
-                                        child: Text(value),
-                                      );
-                                    }).toList(),
-                                    onChanged: (value) =>
-                                        _.changeValueTurn(value),
+                                Expanded(
+                                  flex: 2,
+                                  child: Center(
+                                    child: DropButtonWidget(
+                                      hint: "Turno",
+                                      value: _.valueTurn,
+                                      items: _.valuesTurns.map((value) {
+                                        return DropdownMenuItem(
+                                          value: value,
+                                          child: Text(value),
+                                        );
+                                      }).toList(),
+                                      onChanged: (value) =>
+                                          _.changeValueTurn(value),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: ListView.separated(
-                          itemCount: 15,
-                          separatorBuilder: (context, index) {
-                            return const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 3),
-                            );
-                          },
-                          itemBuilder: (context, index) {
-                            return Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.white,
-                              ),
-                              height: constraints.maxHeight * .05,
-                              child: const ListHoraryWidget(
-                                numTable: "06",
-                                name: "Larissa Silva",
-                                day: "Seg",
-                                turn: "Manhã",
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: constraints.maxHeight * .80,
+                          child: ListView.separated(
+                            itemCount: 15,
+                            separatorBuilder: (context, index) {
+                              return const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 3),
+                              );
+                            },
+                            itemBuilder: (context, index) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                ),
+                                height: constraints.maxHeight * .05,
+                                child: const ListHoraryWidget(
+                                  numTable: "06",
+                                  name: "Larissa Silva",
+                                  day: "Seg",
+                                  turn: "Manhã",
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
---
+++ lib/layers/modules/horary/widgets/list_horary_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ListHoraryWidget extends StatelessWidget {
+  final String? numTable;
+  final String? name;
+  final String? day;
+  final String? turn;
+
+  const ListHoraryWidget({
+    super.key,
+    required this.numTable,
+    required this.name,
+    required this.day,
+    required this.turn,
+  });
+
+  static const textStyle = TextStyle(
+    fontFamily: "Segoe UI",
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: Color(0xff4A6F90),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                  color: Get.theme.colorScheme.primary,
+                ),
+                height: constraints.maxHeight,
+                child: Center(
+                  child: Text(
+                    numTable!,
+                    style: const TextStyle(
+                      fontFamily: "Segoe UI",
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    name!,
+                    overflow: TextOverflow.ellipsis,
+                    style: textStyle,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    day!,
+                    overflow: TextOverflow.ellipsis,
+                    style: textStyle,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    turn!,
+                    overflow: TextOverflow.ellipsis,
+                    style: textStyle,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
