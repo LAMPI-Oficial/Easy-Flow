aa0be27a4e5214af825f84db95b51736eabc0697
RobsonSousaS <robson.silva09@aluno.ifce.edu.br>
2023-05-12 09:27:55 -0300

    listagem de daily

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            toolbarHeight: 80,
+            toolbarHeight: 100,
             leading: IconButton(
               icon: Icon(Icons.arrow_back_ios),
               onPressed: () {},
@@ -32,216 +32,81 @@ class MyApp extends StatelessWidget {
             ),
           ),
           body: Container(
-            alignment: AlignmentDirectional.topCenter,
             color: const Color.fromARGB(245, 245, 245, 255),
             child: Column(
               children: [
                 SizedBox(
                   height: 49,
                 ),
-                Container(
-                  width: 330,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Daily feito em 04 de Janeiro 2023        ',
-                        style: GoogleFonts.notoSans(
-                            textStyle: TextStyle(
-                          color: Color.fromARGB(74, 111, 114, 255),
-                        )),
-                      ),
-                      ElevatedButton(
-                          style: ButtonStyle(
-                            minimumSize:
-                                MaterialStateProperty.all(Size(54, 34)),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'abrir',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  width: 330,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Daily feito em 04 de Janeiro 2023        ',
-                        style: GoogleFonts.notoSans(
-                            textStyle: TextStyle(
-                          color: Color.fromARGB(74, 111, 114, 255),
-                        )),
-                      ),
-                      ElevatedButton(
-                          style: ButtonStyle(
-                            minimumSize:
-                                MaterialStateProperty.all(Size(54, 34)),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'abrir',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  width: 330,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Daily feito em 04 de Janeiro 2023        ',
-                        style: GoogleFonts.notoSans(
-                            textStyle: TextStyle(
-                          color: Color.fromARGB(74, 111, 114, 255),
-                        )),
-                      ),
-                      ElevatedButton(
-                          style: ButtonStyle(
-                            minimumSize:
-                                MaterialStateProperty.all(Size(54, 34)),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'abrir',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  width: 330,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Daily feito em 04 de Janeiro 2023        ',
-                        style: GoogleFonts.notoSans(
-                            textStyle: TextStyle(
-                          color: Color.fromARGB(74, 111, 114, 255),
-                        )),
-                      ),
-                      ElevatedButton(
-                          style: ButtonStyle(
-                            minimumSize:
-                                MaterialStateProperty.all(Size(54, 34)),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'abrir',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  width: 330,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Daily feito em 04 de Janeiro 2023        ',
-                        style: GoogleFonts.notoSans(
-                            textStyle: TextStyle(
-                          color: Color.fromARGB(74, 111, 114, 255),
-                        )),
-                      ),
-                      ElevatedButton(
-                          style: ButtonStyle(
-                            minimumSize:
-                                MaterialStateProperty.all(Size(54, 34)),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'abrir',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  child: FloatingActionButton(
-                    onPressed: () {},
-                    child: const Icon(Icons.add),
-                  ),
-                )
+                Task('Daily feito em 04 de Janeiro 2023        '),
+                Task('Daily feito em 05 de Janeiro 2023        '),
+                Task('Daily feito em 06 de Janeiro 2023        '),
+                Task('Daily feito em 07 de Janeiro 2023        '),
+                Task('Daily feito em 08 de Janeiro 2023        '),
               ],
             ),
           ),
+        floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
         ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: AlignmentDirectional.topCenter,
+      color: const Color.fromARGB(245, 245, 245, 255),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 330,
+            height: 40,
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(8)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  nome,
+                  style: GoogleFonts.notoSans(
+                      textStyle: TextStyle(
+                    color: Color.fromARGB(74, 111, 114, 255),
+                  )),
+                ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(Size(54, 34)),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'abrir',
+                      style: TextStyle(
+                        fontSize: 10,
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
