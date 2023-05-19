57d50a3c4fc9157230651cded238d2deb5780d80
Michael Alves <110749585+Michallves@users.noreply.github.com>
2023-05-19 16:35:33 -0300

    correcao link api temporaria

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // static const String _urlApi = "http://10.12.14.187:8080";
-  static const String _urlApi = "https://be5e-200-17-32-67.ngrok-free.app";
+  static const String _urlApi = "http://165.232.71.169";
 
   static const String urlEndPointAuth = "/auth";
 
