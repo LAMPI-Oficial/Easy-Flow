dea57cb5ffc6118fd1c3c84a0aa9954ff7612fa8
Andeson Silva <andsilva607@gmail.com>
2023-07-17 21:27:22 -0300

    Reiniciando alterações do Home com atualização e pacotes e resolução de um pequeno conflito

--- lib/layers/presentation/ui/pages/equipment/equipment_controller.dart
+++ lib/layers/presentation/ui/pages/equipment/equipment_controller.dart
@@ -8,8 +8,10 @@ class EquipmentController {
     return [
       EquipmentEntity(
           date: "${DateTime.now()}", status: Status.DISAPPROVED, id: 1),
-      EquipmentEntity(date: DateTime.now(), status: Status.APPROVED, id: 2),
-      EquipmentEntity(date: DateTime.now(), status: Status.PENDING, id: 1),
+      EquipmentEntity(
+          date: DateTime.now().toString(), status: Status.APPROVED, id: 2),
+      EquipmentEntity(
+          date: DateTime.now().toString(), status: Status.PENDING, id: 1),
     ];
   }
 
